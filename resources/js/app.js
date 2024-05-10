690243d8d7a1542d2bc42aee352ae7512554b56b
oussama sikaddour <34127545+oussamaSikaddour@users.noreply.github.com>
2024-05-10 16:53:04 +0200

    commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,7 +15,7 @@ import { SPFirstFrom } from './pages/SiteParams.js';
 import { setAriaAttributes } from './traits/Aria.js';
 import { forgetPasswordFirstForm, forgetPasswordSecondForm } from './pages/ForgetPassword.js';
 import { registerFirstForm, registerSecondForm } from './pages/Register.js';
-import { manageUserModalTabs } from './modals/user.js';
+import { manageUserModalTabs } from './modals/User.js';
 import { toggleInertForAllExceptOpenedElement } from './traits/Inert.js';
 
 
