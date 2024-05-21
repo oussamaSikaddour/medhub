544326d830909cf5305a5b1789a4f5da8b4c0a29
oussama sikaddour <34127545+oussamaSikaddour@users.noreply.github.com>
2024-05-21 23:38:02 +0200

    comiit

--- resources/views/components/nav/nav-logo.blade.php
+++ resources/views/components/nav/nav-logo.blade.php
@@ -1,3 +1,3 @@
 <a class="nav__logo" href="{{ route('homePage') }}"  tabindex="0"  aria-label="Description of the overall image" >
-    <img src="{{ asset('img/med.png') }}" alt="app Logo">
+    <img src="{{ asset('img/logo.png') }}" alt="MedHub">
 </a>
